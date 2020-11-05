66ccffe5fbe19eb286d4f13170b9f8bdab2f733a
Pablo Castellanos <pablocd168@gmail.com>
2020-11-05 16:55:11 +0100

    Initial commit

---
+++ webpack.dev.js
@@ -0,0 +1,18 @@
+const path = require("path");
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common");
+const webpack = require("webpack");
+
+module.exports = merge(common, {
+  mode: "development",
+  devServer: {
+    contentBase: path.join(__dirname, "build"),
+    historyApiFallback: true,
+    host: "localhost",
+    compress: true,
+    hot: true,
+    port: 3002,
+    publicPath: "/",
+  },
+  devtool: "source-map",
+});
