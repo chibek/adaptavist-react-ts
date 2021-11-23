889fbfe5582147a221d61bbe4ead847b7f2f7689
chibek <sergibelemarketing@gmail.com>
2021-11-23 20:37:33 +0100

    add order

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,7 +6,11 @@ interface objectWord {
     count: number;
 }
 
-enum order {
+interface objectSort {
+    [words: string]: sortingDirection;
+}
+
+enum sortingDirection {
     ASCENDING = 'ASC',
     DESCENDING = 'DESC',
     DEFAULT = 'DEFAULT',
@@ -17,6 +21,10 @@ const App: FC = () => {
     const [counterWords, setCounterWords] = useState<any>({});
     const [listOfWords, setlistOfWord] = useState<objectWord[]>([]);
     const [initialListOfWords, setInitialListOfWord] = useState<objectWord[]>([]);
+    const [keysSortDirections, setKeysSortDirections] = useState<objectSort>({
+        name: sortingDirection.DEFAULT,
+        count: sortingDirection.DEFAULT,
+    });
 
     const handleChangeInput = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
         setInputText(event.target.value);
@@ -25,7 +33,34 @@ const App: FC = () => {
         setInputText('');
     };
 
-    const orderBy = (key: string) => {};
+    const getNextSortingDirecction = (sortDirection: sortingDirection) => {
+        if (sortDirection === sortingDirection.DEFAULT) {
+            return sortingDirection.DESCENDING;
+        }
+        if (sortDirection === sortingDirection.DESCENDING) {
+            return sortingDirection.ASCENDING;
+        }
+        return sortingDirection.DEFAULT;
+    };
+
+    const orderBy = (key: string) => {
+        const orderDirection = keysSortDirections[key];
+        const nextSortDirection = getNextSortingDirecction(orderDirection);
+        const currentK = { ...keysSortDirections };
+        currentK[key] = nextSortDirection;
+        setKeysSortDirections(currentK);
+        if (nextSortDirection === sortingDirection.DEFAULT) {
+            //return initial state
+        }
+        const sortedList = listOfWords.sort((itemA: any, itemB: any) => {
+            if (itemA[key] > itemB[key]) {
+                return nextSortDirection === sortingDirection.DESCENDING ? -1 : 1;
+            } else if (itemA[key] < itemB[key]) {
+                return nextSortDirection === sortingDirection.DESCENDING ? 1 : -1;
+            }
+            return 0;
+        });
+    };
 
     useEffect(() => {
         const mapWords = inputText
@@ -48,7 +83,6 @@ const App: FC = () => {
                 },
             ]);
         }
-        setInitialListOfWord([...listOfWords]);
     }, [counterWords]);
 
     return (
@@ -57,10 +91,12 @@ const App: FC = () => {
             <table className="bg-blue-200 space-y-2 rounded-lg border border-gray-200 table-auto inline-block shadow-2xl">
                 <thead>
                     <tr className="bg-red-200">
-                        <th className="p-3">
+                        <th className="p-3 cursor-pointer" onClick={() => orderBy('name')}>
                             Words
                         </th>
-                        <th className="p-3">count</th>
+                        <th className="p-3" onClick={() => orderBy('count')}>
+                            count
+                        </th>
                     </tr>
                 </thead>
                 <tbody>
